e8ec26f57c1f07f02d082a8d6c56668322586102
enko <pytlos1337@gmail.com>
2024-05-21 14:54:21 +0200

    Fix register

--- Helpers/SubscriptionHelper.cs
+++ Helpers/SubscriptionHelper.cs
@@ -17,8 +17,10 @@ namespace ChatBot.Helpers
                 int id = Convert.ToInt32(row["id"]);
                 string name = row["name"].ToString();
                 decimal role = Convert.ToDecimal(row["price"]);
-                Subscription.PeriodTypes period = (Subscription.PeriodTypes)row["period"];
-                Subscription.ModelTypes model = (Subscription.ModelTypes)row["model"];
+                string periodString = (string)row["period"];
+                Subscription.PeriodTypes period = (Subscription.PeriodTypes)Enum.Parse(typeof(Subscription.PeriodTypes), periodString);
+                string modelString = (string)row["model"];
+                Subscription.ModelTypes model = (Subscription.ModelTypes)Enum.Parse(typeof(Subscription.ModelTypes), modelString);
 
                 Subscription Subscription = new Subscription(id, name, role, period, model);
                 Subscriptions.Add(Subscription);
