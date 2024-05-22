338c5e684d0eef87e7c0ea84784975abe8a6240f
krystian <kpacholski2001@gmail.com>
2024-05-22 08:13:00 +0200

    fixed

--- Helpers/UserHelper.cs
+++ Helpers/UserHelper.cs
@@ -3,6 +3,8 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Collections.Generic;
 using ChatBot.Models;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace ChatBot.Helpers
 {
@@ -48,5 +50,20 @@ namespace ChatBot.Helpers
 
             return cmd;
         }
+       /* public static HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < hashedBytes.Length; i++)
+                {
+                    builder.Append(hashedBytes[i].ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }*/
     }
 }
